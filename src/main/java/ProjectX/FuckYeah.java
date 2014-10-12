b613dfcd80a19d547d1af878530e2b7ec2e34edb
prahat <prabhatpandey.bits@gmail.com>
2014-10-12 15:43:32 -0400

    final commit

--- src/main/java/ProjectX/FuckYeah.java
+++ src/main/java/ProjectX/FuckYeah.java
@@ -2,7 +2,6 @@ package ProjectX;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +10,12 @@ public class FuckYeah {
 	private static float totalTime = 0;
 
 	public static void main(String[] args) throws IOException {
+		logger.info("Lets startThe Game");
 		gameBegins();
-		logger.info("total number of Tries " + (1 / totalTime));
+		logger.info("total number of Tries " + totalTime);
 	}
 
 	private static int gameBegins() {
-		logger.info("Lets startThe Game");
 		ArrayList<String> aArrayList = new ArrayList<String>();
 		for (int i = 0; i < 10; i++) {
 			int randomNumber = (int) (Math.random() * 200);
@@ -24,10 +23,13 @@ public class FuckYeah {
 		}
 		int thisSum = 0;
 		for (String str : aArrayList) {
-			thisSum += Integer.valueOf(str);
+			try {
+				thisSum += Integer.valueOf(str);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
 		}
-		logger.info("This is the final sum :" + thisSum);
-		if (thisSum <= 1400) {
+		if (thisSum <= 1) {
 			totalTime++;
 			gameBegins();
 		}
