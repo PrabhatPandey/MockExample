0fb4d0390413bc2b876774c578cbc208465c5b7f
Prahat <prabhatpandey.bits@gmail.com>
2014-10-12 10:06:49 -0400

    second commit.

---
+++ src/main/java/ProjectX/FuckYeah.java
@@ -0,0 +1,12 @@
+package ProjectX;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FuckYeah {
+	private static final Logger logger = LoggerFactory.getLogger(FuckYeah.class);
+	public static void main(String[] args) {
+		logger.info("Go fuck yourself!");
+		logger.info("I am back to bang!");
+	}
+}
