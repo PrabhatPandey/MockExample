3ccb52b8c2fd35c6e8eb149ccb601e0b5e4ecfeb
prahat <prabhatpandey.bits@gmail.com>
2014-11-10 08:29:20 -0500

    Priority queue is added.

---
+++ src/main/java/priorityqueue/PriorityQueueTest.java
@@ -0,0 +1,30 @@
+package priorityqueue;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.PriorityQueue;
+
+public class PriorityQueueTest {
+
+	public static void main(String[] args) {
+
+		PriorityQueue<GregorianCalendar> pq = new PriorityQueue<GregorianCalendar>();
+		pq.add(new GregorianCalendar(1906, Calendar.DECEMBER, 9));
+		pq.add(new GregorianCalendar(1815, Calendar.DECEMBER, 10));
+		pq.add(new GregorianCalendar(1903, Calendar.DECEMBER, 9));
+		pq.add(new GregorianCalendar(1910, Calendar.DECEMBER, 9));
+
+		System.out.println("Iterating over elements...");
+		for (GregorianCalendar date : pq) {
+			System.out.println(date.get(Calendar.YEAR));
+			System.out.println("removing the elements");
+			while (!pq.isEmpty())
+				System.out.println(pq.remove().get(Calendar.YEAR));
+		}
+		if (pq.isEmpty()) {
+			System.out.println("Queue is empty");
+		}
+
+	}
+
+}
