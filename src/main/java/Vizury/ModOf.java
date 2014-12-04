1fe0bd0a0d01f0aed81576772d30187974bbd565
prahat <prabhatpandey.bits@gmail.com>
2014-12-04 04:51:08 -0500

    removed the for loop

--- src/main/java/Vizury/ModOf.java
+++ src/main/java/Vizury/ModOf.java
@@ -1,40 +1,36 @@
 package Vizury;
 
-
 public class ModOf {
 
 	public static void main(String[] args) {
-		int remainder = 0;
 		int valueToAdd = 0;
 		int index = 0;
-		String Input = "142000";
+		String Input = "2222222222773443333333333312";
 
-		int number = 91;
+		int number = 233;
 		System.out.println(getTheMod(Input, number, index, valueToAdd));
 	}
 
 	private static int getTheMod(String input, int number, int j, int valueToDevided) {
 
 		int mod = 0;
-		for (int i = j; i < input.length(); i++) {
-
-			String getThisChar = "" + input.charAt(i);
+		while (j < input.length()) {
+			String getThisChar = "" + input.charAt(j);
 			valueToDevided = valueToDevided * 10 + Integer.parseInt(getThisChar);
 
 			if (valueToDevided < number) {
-				getTheMod(input, number, (i + 1), valueToDevided);
+				getTheMod(input, number, (j + 1), valueToDevided);
 				mod = valueToDevided;
 			} else {
 				mod = valueToDevided % number;
 				valueToDevided = mod;
-				while (input.length() > i) {
-					getTheMod(input, number, (i + 1), valueToDevided);
+				while (input.length() > j) {
+					getTheMod(input, number, (j + 1), valueToDevided);
 					break;
 				}
 			}
-
+			j++;
 		}
-
 		return mod;
 	}
 }
